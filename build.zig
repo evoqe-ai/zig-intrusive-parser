d984bd0371dc813562274defd4669fe6519461c5
Enrico de Klerk <enrico@hub9.com>
2025-01-06 21:43:21 +1100

    Add package manager config

--- build.zig
+++ build.zig
@@ -1,6 +1,21 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const extras = b.dependency("extras", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const intrusive_parser = b.addModule("intrusive-parser", .{
+        .target = target,
+        .optimize = optimize,
+        .root_source_file = b.path("intrusive_parser.zig"),
+    });
+    intrusive_parser.addImport("extras", extras.module("extras"));
+
     const test_step = b.step("test", "Run unit tests");
     _ = test_step;
 }
